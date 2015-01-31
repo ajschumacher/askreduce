db76f4ca328800649a9e362eb74a3d29a48ed7f3
Aaron Schumacher <ajschumacher@gmail.com>
2015-01-31 17:29:47 -0500

    add Question and Answer models

--- shufflesort/models.py
+++ shufflesort/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+"""
+There is currently only one space of questions.
+Users are only strings; the plan is to associate with sessions.
+"""
+
+
+class Question(models.Model):
+    user = models.CharField(max_length=32)
+    text = models.TextField()
+    date = models.DateTimeField()
+
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question)
+    user = models.CharField(max_length=32)
+    text = models.TextField()
+    date = models.DateTimeField()
