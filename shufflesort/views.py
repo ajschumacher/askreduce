3a0d2485771938a95a9a67adcd3ed8e023319323
Aaron Schumacher <ajschumacher@gmail.com>
2015-01-31 23:20:22 -0500

    use get_object_or_404

--- shufflesort/views.py
+++ shufflesort/views.py
@@ -1,5 +1,4 @@
-from django.http import Http404
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, render
 
 from shufflesort.models import Question
 
@@ -11,9 +10,6 @@ def index(request):
 
 
 def question(request, question_id):
-    try:
-        question = Question.objects.get(pk=question_id)
-    except Question.DoesNotExist:
-        raise Http404("Question does not exist")
+    question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
     return render(request, 'shufflesort/question.html', context)
