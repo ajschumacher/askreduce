51a67dd535105305bd082a118cc41ec9340939a9
Aaron Schumacher <ajschumacher@gmail.com>
2015-02-01 18:56:38 -0500

    a sort of authentication

--- shufflesort/views.py
+++ shufflesort/views.py
@@ -1,17 +1,31 @@
 from django.http import HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.shortcuts import get_object_or_404, render
+import functools
 from django.views.generic import View
 
 from shufflesort.models import Question
 
 
+def with_identity(func):
+    @functools.wraps(func)
+    def newfunc(request, **kwargs):
+        if 'user' in request.session:
+            return func(request, **kwargs)
+        else:
+            request.session['target'] = request.path
+            return HttpResponseRedirect(reverse('shufflesort:identity'))
+    return newfunc
+
+
+@with_identity
 def index(request):
     question_list = Question.objects.order_by('date')
     context = {'question_list': question_list}
     return render(request, 'shufflesort/index.html', context)
 
 
+@with_identity
 def question(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
     context = {'question': question}
@@ -29,4 +43,8 @@ class IdentityView(View):
             request.session['user'] = request.POST['user']
         if 'deidentify' in request.POST:
             request.session.flush()
+        if 'target' in request.session:
+            target = request.session['target']
+            del request.session['target']
+            return HttpResponseRedirect(target)
         return HttpResponseRedirect(reverse('shufflesort:identity'))
