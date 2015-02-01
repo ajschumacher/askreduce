6917a76708ae5b38888fa9ee8ef309f01b6d8ca1
Aaron Schumacher <ajschumacher@gmail.com>
2015-01-31 22:53:38 -0500

    trailing slashes are cool?

--- shufflesort/urls.py
+++ shufflesort/urls.py
@@ -5,5 +5,5 @@ from shufflesort import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^question/(?P<question_id>\d+)$', views.question, name='question'),
+    url(r'^question/(?P<question_id>\d+)/$', views.question, name='question'),
 )
