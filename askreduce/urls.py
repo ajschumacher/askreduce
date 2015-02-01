b3ebbfa158dff7059cfc32e02290ace110b0a857
Aaron Schumacher <ajschumacher@gmail.com>
2015-01-31 23:28:33 -0500

    Namespaces are one honking great idea -- let's do more of those!

--- askreduce/urls.py
+++ askreduce/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^', include('shufflesort.urls')),
+    url(r'^', include('shufflesort.urls', namespace='shufflesort')),
     url(r'^admin/', include(admin.site.urls)),
 )
