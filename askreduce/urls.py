fdba5504a4e8d0e2e0f60ced3451f9833cb47968
Aaron Schumacher <ajschumacher@gmail.com>
2015-01-31 22:36:59 -0500

    don't play; shufflesort is on the root

--- askreduce/urls.py
+++ askreduce/urls.py
@@ -2,6 +2,6 @@ from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 urlpatterns = patterns('',
-    url(r'^shufflesort/', include('shufflesort.urls')),
+    url(r'^$', include('shufflesort.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
