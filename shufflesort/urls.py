e3e4a6fb97988f7d7c7737912d96225c92765635
Aaron Schumacher <ajschumacher@gmail.com>
2015-02-01 19:40:18 -0500

    all class-based views

--- shufflesort/urls.py
+++ shufflesort/urls.py
@@ -4,7 +4,13 @@ from shufflesort import views
 
 
 urlpatterns = patterns('',
-    url(r'^$', views.index, name='index'),
-    url(r'^identity$', views.IdentityView.as_view(), name='identity'),
-    url(r'^question/(?P<question_id>\d+)/$', views.question, name='question'),
+    url(r'^$',
+        views.IndexView.as_view(),
+        name='index'),
+    url(r'^identity$',
+        views.IdentityView.as_view(),
+        name='identity'),
+    url(r'^question/(?P<question_id>\d+)/$',
+        views.QuestionView.as_view(),
+        name='question'),
 )
--- shufflesort/views.py
+++ shufflesort/views.py
@@ -9,27 +9,29 @@ from shufflesort.models import Question
 
 def with_identity(func):
     @functools.wraps(func)
-    def newfunc(request, **kwargs):
+    def newfunc(self, request, **kwargs):
         request.session['target'] = request.path
         if 'user' in request.session:
-            return func(request, **kwargs)
+            return func(self, request, **kwargs)
         else:
             return HttpResponseRedirect(reverse('shufflesort:identity'))
     return newfunc
 
 
-@with_identity
-def index(request):
-    question_list = Question.objects.order_by('date')
-    context = {'question_list': question_list}
-    return render(request, 'shufflesort/index.html', context)
+class IndexView(View):
+    @with_identity
+    def get(self, request):
+        question_list = Question.objects.order_by('date')
+        context = {'question_list': question_list}
+        return render(request, 'shufflesort/index.html', context)
 
 
-@with_identity
-def question(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    context = {'question': question}
-    return render(request, 'shufflesort/question.html', context)
+class QuestionView(View):
+    @with_identity
+    def get(self, request, question_id):
+        question = get_object_or_404(Question, pk=question_id)
+        context = {'question': question}
+        return render(request, 'shufflesort/question.html', context)
 
 
 class IdentityView(View):
