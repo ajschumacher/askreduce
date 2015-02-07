bd15d914dcceebd12e5578def2811e99ffee3e40
Aaron Schumacher <ajschumacher@gmail.com>
2015-02-07 17:22:08 -0500

    markdown for questions

---
+++ shufflesort/templatetags/markdown_filter.py
@@ -0,0 +1,9 @@
+from django import template
+import markdown
+
+register = template.Library()
+
+@register.filter
+def markdown_filter(text):
+    # safe_mode governs how the function handles raw HTML
+    return markdown.markdown(text, safe_mode='escape')
